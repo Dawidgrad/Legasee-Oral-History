2097b8a97280b7005cbb0bd96caad640e3dc042b
Robert Flynn <wobrob101@gmail.com>
2021-12-08 15:31:29 +0000

    CTC Beam search algorithm for use with TransformerXL
    Needs optimising currently very slow and cannot handle sequence longer than 900 steps due to recursion limit

---
+++ Language_models/Transformer-XL/Beam_Search_CTC/adapter.py
@@ -0,0 +1,157 @@
+import torch
+
+
+# TransformerXL vocab
+vocabXL = {
+    "'": 24,
+    '<pad>': 0,
+    '<unk>': 1,
+    'A': 4,
+    'B': 21,
+    'C': 16,
+    'D': 11,
+    'E': 2,
+    'F': 17,
+    'G': 18,
+    'H': 8,
+    'I': 7,
+    'J': 26,
+    'K': 23,
+    'L': 12,
+    'M': 14,
+    'N': 6,
+    'O': 5,
+    'P': 20,
+    'Q': 27,
+    'R': 10,
+    'S': 9,
+    'T': 3,
+    'U': 13,
+    'V': 22,
+    'W': 15,
+    'X': 25,
+    'Y': 19,
+    'Z': 28,
+    ' ': 29
+}
+
+#vocab for wav2vec
+wav2vecVocab = {
+ '<pad>': 0,
+ '<s>': 1,
+ '</s>': 2,
+ '<unk>': 3,
+ ' ': 4,
+ 'E': 5,
+ 'T': 6,
+ 'O': 7,
+ 'A': 8,
+ 'I': 9,
+ 'N': 10,
+ 'H': 11,
+ 'S': 12,
+ 'R': 13,
+ 'L': 14,
+ 'D': 15,
+ 'U': 16,
+ 'Y': 17,
+ 'W': 18,
+ 'M': 19,
+ 'C': 20,
+ 'G': 21,
+ 'F': 22,
+ 'P': 23,
+ 'B': 24,
+ 'K': 25,
+ "'": 26,
+ 'V': 27,
+ 'J': 28,
+ 'X': 29,
+ 'Q': 30,
+ 'Z': 31
+ }
+
+
+class TransfoXLPredictonHead():
+  '''
+    Takes in a sequence and outputs a probability distrubution over the next character
+    outputs standard probability rather than logs
+  '''
+  def __init__(self, model):
+    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    self.model = model.to(self.device)
+    self.model.eval()
+    self.weight = model.crit.out_layers[0].weight
+    self.bias = model.crit.out_layers[0].bias
+    self.out_projs = model.crit.out_projs[0]  
+    self.compute_logit = model.crit._compute_logit
+
+  def __call__(self, input:torch.tensor, indices:torch.tensor):
+    return self.forward(input, indices)
+
+  def forward(self, input:torch.tensor, indices:torch.tensor): #, mems=None): not added functionality for mems yet
+    if(len(input.shape) == 1):
+      input = input.unsqueeze(0) # add dimension if needed
+    with torch.no_grad():
+      out = self.model.transformer(input.to(self.device), mems=None, output_hidden_states=True).last_hidden_state
+  
+      out = out[torch.tensor([el for el in range(out.shape[0])]).to(self.device), indices.to(self.device)]  # select indices for token inputs i.e we don't need predictions for padding
+    
+      logits = self.compute_logit(out, self.weight, self.bias, self.out_projs)
+      probs = torch.nn.functional.softmax(logits, -1).cpu().numpy()
+    return probs #return smax over next char in seq
+
+
+def convertdict():
+  cvrtdict = {}
+  for item in wav2vecVocab.items():
+    k, v = item
+    if k in vocabXL:
+      cvrtdict[v] = vocabXL[k]
+    else:
+      cvrtdict[v] = None
+  return cvrtdict
+
+def convert(output):
+  '''
+    Converts the output of the model to match the language models vocab before softmax is applied
+  '''
+  vocab_convert = convertdict()
+  target = torch.empty((output.shape[0], len(vocabXL.values())), dtype=torch.float32)
+  for i, step in enumerate(output):
+    for j, item in enumerate(step):
+      if vocab_convert[j] is not None:
+        target[i][vocab_convert[j]] = item
+  return target
+
+class Wav2Vec2CTCPredictionHead():
+  def __init__(self, model):
+    self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    self.model = model.to(self.device)
+    self.model.eval()
+    self.vocab_convert = convertdict()
+
+    
+  def __call__(self):
+    return self.forward()
+
+
+  def _convert(self, output): #DOESN'T WORK
+    '''
+      Converts the output of the model to match the language models vocab before softmax is applied
+    '''
+    target = torch.empty((output.shape[0], len(vocabXL.values())), dtype=torch.float32)
+    for i, step in enumerate(output):
+      for j, item in enumerate(step):
+        if self.vocab_convert[j] is not None:
+          target[i][self.vocab_convert[j]] = item
+    return target
+
+  def __forward__(self, input):
+    '''
+      Forward pass of the model, convert the output to match the language models vocab and apply softmax
+    '''
+    with torch.no_grad():
+      out = self.model(input.to(self.device)).squeeze().cpu().numpy()
+    convert = self._convert(out)
+    return torch.nn.functional.softmax(convert, -1)
---
+++ Language_models/Transformer-XL/Beam_Search_CTC/beam_utils.py
@@ -0,0 +1,240 @@
+from typing import Dict
+import numpy as np
+import torch
+from tqdm import tqdm
+
+# Optimization
+# dictionary lookup is 6.6 times faster than a list, so replace all lists with dicts where possible
+# Remove redundant looping of lists
+# Improve beam collapsing
+# Replace recursive calls with a loop
+# After certain sequence length store LM models hidden states in memory and use them for the next sequence
+# Replace looped multiplication with matrix operations where possible
+# cache beam strings
+# work in log space where possible addition > multiplication
+
+class Beam():
+    '''
+    Manages each instance of a beam
+    '''
+    def __init__(self, seq, score, pad_tkn=0, space_tkn=29):
+        self.seq = seq
+        self.score = score
+        self.pad_tkn = pad_tkn
+        self.space_tkn = space_tkn
+        self.next_logits = None
+
+    def add(self, token, score):
+        self.seq.append(token)
+        self.score = self.score * score
+        self.collapse() 
+
+    def collapse(self): # This should be optimized to avoid collapsing parts of the sequence that have already been collapsed
+        seq = []
+        for i, tkn in enumerate(self.seq):   # This doesn't account for spaces with a padding token between them # Fixed
+            if len(seq) == 0: #first token in sequence cannot be a space or pad token
+                if tkn != self.space_tkn and tkn != self.pad_tkn:   
+                    seq.append(tkn)
+            elif tkn != seq[-1]: #if the current token is not the same as the previous token then add it to the sequence
+                if tkn == self.space_tkn and i > 1 and self.seq[i-1] == self.pad_tkn and self.seq[i-2] == self.space_tkn:
+                    pass # this fixes the double space issue.. probably a better way to do all this
+                else: 
+                    seq.append(tkn)
+        self.seq = seq
+
+    def __str__(self):
+        '''
+        Returns a string representation of the beam
+        '''
+        return "".join(map(str, self.seq))
+
+    def __len__(self):
+        return len(self.seq)
+
+    def get_seq(self):
+        return self.seq
+
+class BeamSearch():
+    '''
+    - CTC beam search algorithm
+    - Vocab for LM must be the same as vocab for ASR
+    - Beam_width must be computatable as one batch of inputs to the LM model
+    '''
+    def __init__(self, beam_width:int, asr_logits:np.array, vocab:Dict, lm_model, lm_weight=0.5, pad_tkn=0, space_tkn=29):
+        self.beam_width = beam_width # max number of hypotheses to be considered at each time step
+        self.vocab = vocab 
+        self.reverse_vocab = {v:k for k,v in vocab.items()} 
+        self.asr_logits = asr_logits # Wav2vec2 logits [seq_len, vocab_size]
+        self.pad_tkn = pad_tkn       # token used for padding
+        self.space_tkn = space_tkn   # token used for space
+        self.lm = lm_model           # LM model __call__ must return softmax over vocab [vocab_size]
+        self.beams = []              # list of all beam objects
+        self.lm_weight = lm_weight   # weight for language model
+        self.asr_weight = 1 - self.lm_weight #weight of the asr model
+
+        self.current_pos = 0         # current position of the search within the logits
+
+        self.loading_bar = None      # progress bar (tqdm)
+
+        self.cache = {}              # cache for lm logits that have already been computed
+        self.debug = False         
+
+        if self.lm_weight == 0:
+            self.lm = DummyLM()      # if lm_weight is 0 then perform greedy search
+
+    def _get_sorted(self, current_pos):
+        '''
+        Returns array where the index corresponds to the order of sorted logits i.e [0] = most probable, [-1] = least probable
+        and the value is the number corresponding to a character in the vocabulary
+        '''
+        sorted_ = (-current_pos).argsort()
+        beam_order = np.empty(sorted_.shape,dtype=np.int8)
+        for i, el in enumerate(sorted_):
+            beam_order[el] = i
+        return beam_order
+
+    def _init_beams(self):
+        beam_order =  self._get_sorted(self.asr_logits[self.current_pos])
+        for char in beam_order:
+            if len(self.beams) >= self.beam_width:
+                break
+            self.beams.append(Beam([char], self.asr_logits[self.current_pos, char]))
+        for beam in self.beams:
+            beam.collapse()
+            
+
+    def _convert_to_text(self, seq):
+        return "".join([self.reverse_vocab[el] for el in seq if el != self.pad_tkn])
+
+    def _return_beams(self):
+        return [( self._convert_to_text(beam.get_seq()), beam.score ) for beam in self.beams]
+    
+
+    def _get_beam_str(self, beam):
+        '''
+        String representation of the beam, ignoring the pad tokens
+        '''
+        beam_seq = beam.get_seq() 
+        return "".join([str(el) for el in beam_seq if el != self.pad_tkn]) #string identifier for the beam, padding tokens are not included in the identifier
+    
+    def _merge_beams(self, beams): 
+        '''
+        Merge identical beams
+        '''
+        cbeams = [] # list to hold the merged beams
+        blist = []  # list to hold string identifiers for the beams that have already been stored
+        for beam in beams:
+            beam_str = self._get_beam_str(beam)
+            if beam_str not in blist:
+                cbeams.append(beam)
+                blist.append(beam_str)
+            else:
+                cbeams[blist.index(beam_str)].score += beam.score #add the score of the new beam to the existing beam
+        return cbeams
+
+
+    def _retrieve_batch(self):
+        '''
+        Updates the next_logits for each beam in the beam list
+        '''
+        def pad_batch(seq): # pad the batch to max seq length and returns a mask to be passed to the model
+            max_len = max([len(el) for el in seq])
+            tgt_indices = torch.empty(len(seq), dtype=torch.long) # target indices to select lm predictions from i.e we want the last token that isn't padding
+            for i, el in enumerate(seq):
+                tgt_indices[i] = len(el) - 1
+                el.extend([self.pad_tkn]*(max_len-len(el)))
+            return torch.tensor(seq, dtype=torch.long), tgt_indices 
+
+        sequences = []      # store sequences to be passed to the LM
+        beam_indices = []   # indices of the sequences corresponding to the beam
+        batch_indices = []  # indices of the sequences corresponding to the batch
+        for i, beam in enumerate(self.beams):
+            cur_seq = beam.get_seq() 
+            cur_seq = [el for el in cur_seq if el != self.pad_tkn] # remove padding tokens from the sequence, before passing to the LM
+            cur_seq_str = self._get_beam_str(beam) # change this maybe as this is performing a similar function to the above
+
+            if len(cur_seq) == 0:          # hmm should add start token to lm model during training for this
+                cur_seq = [self.space_tkn] # if the sequence is empty then add a space token
+                batch_indices.append(len(sequences))
+                sequences.append(cur_seq)
+                beam_indices.append(i)
+
+            elif cur_seq_str not in self.cache:
+                batch_indices.append(len(sequences))
+                sequences.append(cur_seq)
+                beam_indices.append(i)
+
+            else:
+                print('cache hit') if self.debug else None
+                beam.next_logits = self.cache[cur_seq_str]
+
+        if len(sequences) != 0:
+            sequences, tgt_indices = pad_batch(sequences)
+            batch_logits = self.lm(sequences, tgt_indices)
+
+            for i_beam, i_batch in zip(beam_indices, batch_indices):
+                self.beams[i_beam].next_logits = batch_logits[i_batch]      # update the next_logits for each beam
+                self.cache[str(self.beams[i_beam])] = batch_logits[i_batch] # cache the logits
+
+     
+
+
+    def _get_candidate(self, beam:Beam):
+        '''
+        Returns list of candidate next beams. Every item in the vocabulary is a potential candidate for the search
+        '''
+        if beam.next_logits is None:
+            print('Next logits is None, this should not happen')
+            return [Beam([*beam.get_seq(), token], beam.score*self.asr_logits[self.current_pos, token]) for token in self.vocab.values()]
+        else:
+            return [Beam([*beam.get_seq(),token], beam.score*( self.lm_weight*beam.next_logits[token] + self.asr_weight*self.asr_logits[self.current_pos, token] ) ) for token in self.vocab.values()] #compute the score for each candidate beam, combining the score from the language model and the score from the asr model
+
+    def _search_step(self):
+        candidate_sets = [self._get_candidate(beam) for beam in self.beams]
+        candidate_beams = [item for sublist in candidate_sets for item in sublist] #unpack the candidate sets
+        for beam in candidate_beams:
+            beam.collapse()
+        candidate_beams = self._merge_beams(candidate_beams)
+        candidate_beams.sort(key=lambda beam: beam.score, reverse=True) #sort by score (largest to smallest)
+      
+        self.beams = candidate_beams[:self.beam_width] #prune to beam_width
+
+    def _loading_bar(self, end=False):
+        if self.loading_bar is None:
+            self.loading_bar = tqdm(total=self.asr_logits.shape[0]-1, desc="Searching..")
+            self.loading_bar.update()
+        elif end:
+            self.loading_bar.close()
+            self.loading_bar = None
+        else:
+            self.loading_bar.update()
+
+    def search(self): #main search function that returns the top k beams
+        ### End of sequence 
+        if self.current_pos == len(self.asr_logits) - 1:
+            self._loading_bar(end=True)
+            return [self, self._return_beams()] if self.debug else self._return_beams()
+        ## Start of sequence
+        elif len(self.beams) == 0:
+            self._init_beams()
+        # During sequence
+        else:
+            self._search_step()
+
+        self._retrieve_batch() #retrieve logits for next character for each beam
+        self.current_pos += 1
+        self._loading_bar()
+        return self.search() #recursive call
+    
+
+            
+class DummyLM():
+    '''
+    Dummy Language Model for greedy search when LM weight is 0
+    '''
+    def __init__(self, vocab_size=30):
+        self.vocab_size = vocab_size
+    def __call__(self, batch:torch.tensor, indices:torch.tensor):
+        out = np.ones((batch.shape[0], self.vocab_size))
+        return out
+

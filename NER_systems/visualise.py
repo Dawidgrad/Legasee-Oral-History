f4c48b1f9d98c65d0f5e85030ec0c4f9afc7b431
dawidgrad <dawidgrad.work@gmail.com>
2022-01-10 22:25:38 +0000

    Code clean-up

--- NER_systems/visualise.py
+++ NER_systems/visualise.py
@@ -2,6 +2,37 @@ import docx
 from docx.enum.text import WD_COLOR_INDEX
 from utilities import get_transcript
 
+def read_results(path):
+    results = list()
+    entities = list()
+
+    with open(path) as file:
+        line = file.readline()
+        while line != '':
+            if line == 'batch_end\n':
+                results.append(entities)
+                line = file.readline()
+                entities = list()
+                continue
+            entities.append(eval(line))
+            line = file.readline()
+
+    return results
+
+def write_to_doc(transcript, results):
+    for batch, entities in zip(transcript, results):
+        para = doc.add_paragraph()
+        prev_idx = 0
+
+        for entity in entities:
+            start_idx = entity[0][0]
+            end_idx = entity[0][1]
+
+            para.add_run(batch[prev_idx:start_idx])
+            para.add_run(batch[start_idx:end_idx]).font.highlight_color = WD_COLOR_INDEX.YELLOW
+            prev_idx = end_idx
+
+
 ################################################################
 # Main Function
 
@@ -12,62 +43,28 @@ if __name__ == '__main__':
 
     doc = docx.Document()
 
-    results = list()
-    # Read the GATE results in
-    with open('./spacy_results.txt') as file:
-        line = file.readline()
-        entities = list()
-        while line != '':
-            if line == 'batch_end\n':
-                results.append(entities)
-                line = file.readline()
-                entities = list()
-                continue
-            entities.append(eval(line))
-            line = file.readline()
-
     # Visualise GATE results
+    results = read_results('./gate_results.txt')
     doc.add_heading('GATE', 0)
-
-    for batch, entities in zip(transcript, results):
-        para = doc.add_paragraph()
-        prev_idx = 0
-        
-        for entity in entities:
-            start_idx = entity[0][0]
-            end_idx = entity[0][1]
-
-            para.add_run(batch[prev_idx:start_idx])
-            para.add_run(batch[start_idx:end_idx]).font.highlight_color = WD_COLOR_INDEX.YELLOW
-            prev_idx = end_idx
+    write_to_doc(transcript, results)
     
-    
-
-
     # Visualise Flair results
+    results = read_results('./flair_results.txt')
     doc.add_heading('Flair', 0)
-    
-    para = doc.add_paragraph('''GeeksforGeeks is a Computer Science portal for geeks.''')
-    para.add_run(''' It contains well written, well thought and well-explained '''
-                ).font.highlight_color = WD_COLOR_INDEX.YELLOW
-    para.add_run('''computer science and programming articles, quizzes etc.''')
+    write_to_doc(transcript, results)
 
-    
     # Visualise spaCy results
+    results = read_results('./spacy_results.txt')
     doc.add_heading('spaCy', 0)
-    
-    para = doc.add_paragraph('''GeeksforGeeks is a Computer Science portal for geeks.''')
-    para.add_run(''' It contains well written, well thought and well-explained '''
-                ).font.highlight_color = WD_COLOR_INDEX.YELLOW
-    para.add_run('''computer science and programming articles, quizzes etc.''')
+    write_to_doc(transcript, results)
 
-    
     # Visualise Stanford results
+    results = read_results('./stanford_results.txt')
     doc.add_heading('Stanford', 0)
-    
-    para = doc.add_paragraph('''GeeksforGeeks is a Computer Science portal for geeks.''')
-    para.add_run(''' It contains well written, well thought and well-explained '''
-                ).font.highlight_color = WD_COLOR_INDEX.YELLOW
-    para.add_run('''computer science and programming articles, quizzes etc.''')
-    
+    write_to_doc(transcript, results)
+
+    # Visualise DeepPavlov results
+    # TODO
+
+    # Save the document
     doc.save('highlighted_entities.docx')

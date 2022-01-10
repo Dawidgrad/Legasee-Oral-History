0bd5ad369eeb8a10e8fa0619dd6ff9c117e3b58f
dawidgrad <dawidgrad.work@gmail.com>
2022-01-10 22:15:56 +0000

    Visualisation of the NER results

--- NER_systems/visualise.py
+++ NER_systems/visualise.py
@@ -12,20 +12,36 @@ if __name__ == '__main__':
 
     doc = docx.Document()
 
+    results = list()
     # Read the GATE results in
-
+    with open('./spacy_results.txt') as file:
+        line = file.readline()
+        entities = list()
+        while line != '':
+            if line == 'batch_end\n':
+                results.append(entities)
+                line = file.readline()
+                entities = list()
+                continue
+            entities.append(eval(line))
+            line = file.readline()
 
     # Visualise GATE results
     doc.add_heading('GATE', 0)
 
-    for batch in transcript:
-        # 
-        print()
+    for batch, entities in zip(transcript, results):
+        para = doc.add_paragraph()
+        prev_idx = 0
+        
+        for entity in entities:
+            start_idx = entity[0][0]
+            end_idx = entity[0][1]
+
+            para.add_run(batch[prev_idx:start_idx])
+            para.add_run(batch[start_idx:end_idx]).font.highlight_color = WD_COLOR_INDEX.YELLOW
+            prev_idx = end_idx
     
-    para = doc.add_paragraph('''GeeksforGeeks is a Computer Science portal for geeks.''')
-    para.add_run(''' It contains well written, well thought and well-explained '''
-                ).font.highlight_color = WD_COLOR_INDEX.YELLOW
-    para.add_run('''computer science and programming articles, quizzes etc.''')
+    
 
 
     # Visualise Flair results

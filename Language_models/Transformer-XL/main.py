560435ae0e295e38fe4ed2ed3df121b2a4478116
Robert Flynn <wobrob101@gmail.com>
2022-01-11 13:00:34 +0000

    Updated TransformerXL to work with batches > 1

--- Language_models/Transformer-XL/main.py
+++ Language_models/Transformer-XL/main.py
@@ -1,19 +1,53 @@
 import argparse
+from numpy import split
 import torch
 from torch.utils.data import dataloader
 import model_utils
 import data_utils
 from os import listdir as ls
+import pandas as pd
 
 def main(args):
+    # dataloader uses a folder of text files to create a dataset
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataloader = data_utils.get_dataloader(ls(args.train_dir), data_utils.tokenize, args.batch_size, args.train_dir, True, 1)
+    train_test_val = pd.read_csv(args.train_test_val)
+
+    train_dataloader = data_utils.get_dataloader(
+        csv=train_test_val, 
+        tokenizer = data_utils.tokenize, 
+        batch_size = args.batch_size, 
+        split='train', 
+        shuffle=True, 
+        num_workers=1
+    )
+    test_dataloader = data_utils.get_dataloader(
+        csv=train_test_val, 
+        tokenizer = data_utils.tokenize, 
+        batch_size = args.batch_size, 
+        split='train', 
+        shuffle=True, 
+        num_workers=1
+    )
+    val_dataloader = data_utils.get_dataloader(
+        csv=train_test_val, 
+        tokenizer = data_utils.tokenize, 
+        batch_size = args.batch_size, 
+        split='train', 
+        shuffle=True, 
+        num_workers=1
+    )
+    dataloaders = {
+        'train': train_dataloader,
+        'val': val_dataloader,
+        'test': test_dataloader
+    }
+
     print('--- Data loaded ---')
     if args.ckpt == False:
         model = model_utils.return_untrained_model()
         model.to(device)
         print('--- Model Loaded ---')
-        trainer = model_utils.Trainer(model=model, epochs=args.epochs, trainloader=dataloader, batch_chunks=args.batch_chunks, lr=args.lr)
+        trainer = model_utils.Trainer(model=model, epochs=args.epochs, dataloaders=dataloaders, batch_chunks=args.batch_chunks, accum=args.accum, lr=args.lr)
         print('--- Trainer Loaded ---')
         print('Total Number of parameters: ', trainer.model.num_parameters())
         trainer.fit()
@@ -22,11 +56,12 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=1, help="number of epochs")
-    parser.add_argument("--batch_chunks", type=int, default=1000, help="size of chunks to split batch sequences into")
-    parser.add_argument('--batch_size', type=int, default=1, help='batch size') #note only working for batch size of 1 atm because of bug with hugging face
-    parser.add_argument('--lr', type=float, default=0.00025, help="learning rate")
+    parser.add_argument("--batch_chunks", type=int, default=500, help="size of chunks to split batch sequences into")
+    parser.add_argument('--batch_size', type=int, default=5, help='batch size')  
+    parser.add_argument('--lr', type=float, default=0.0002, help="learning rate")
     parser.add_argument('--ckpt', type=bool, default=False, help="whether to load from a checkpoint")
     parser.add_argument('--ckpt_path', type=str, default="", help="path to checkpoint")
-    parser.add_argument('--train_dir', type=str, default="./data/", help="path to training data")
+    parser.add_argument('--accum', type=str, default=4, help='gradient accumulation')
+    parser.add_argument('--data_csv', type=str, default="train_test_val.csv", help="path to data csv file")
     args = parser.parse_args()
     main(args)
--- Language_models/Transformer-XL/model_utils.py
+++ Language_models/Transformer-XL/model_utils.py
@@ -1,6 +1,18 @@
+from typing import Dict
 import torch
 from transformers import TransfoXLTokenizer, TransfoXLLMHeadModel, TransfoXLConfig
 from tqdm import tqdm
+import transformers
+import subprocess as sp
+import os
+
+def get_gpu_memory(): # https://stackoverflow.com/questions/59567226/how-to-programmatically-determine-available-gpu-memory-with-tensorflow
+    command = "nvidia-smi --query-gpu=memory.free --format=csv"
+    memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
+    memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
+    return memory_free_values
+
+
 
 config = TransfoXLConfig(
     d_embed=256,
@@ -14,58 +26,115 @@ config = TransfoXLConfig(
     adaptive=False,
 )
 
+
+
 def return_untrained_model():
+    # set model.crit to -> torch.nn.CrossEntropyLoss() to allow for masked loss w/ -100 as padding
+    # no need for adaptive softmax w/ vocab_size=30
     model = TransfoXLLMHeadModel(config)
+    model.crit = torch.nn.CrossEntropyLoss()
+    model.out_layer = torch.nn.Linear(config.d_model, config.vocab_size)
     return model
 
 
 class Trainer():
-    def __init__(self, model:TransfoXLLMHeadModel, epochs:int, trainloader:torch.utils.data.DataLoader, batch_chunks:int, lr:float):
+    def __init__(self, model:TransfoXLLMHeadModel, epochs:int, dataloaders:Dict, batch_chunks:int, accum:int, lr:float, save_folder:str='./chechkpoints'):
       self.model = model
       self.epochs = epochs
-      self.trainloader = trainloader
+      
+      self.trainloader = dataloaders['train']
+      self.valloader = dataloaders['val']
+      self.testloader = dataloaders['test']
+
       self.b_chunks = batch_chunks
       self.lr = lr
       self.optim = torch.optim.Adam(self.model.parameters(), lr=self.lr)
       self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
       self.losses = []
+      self.accum = accum
 
-    def onTrainEnd(self):
+
+    def __forward(self, inputs, labels):
+            transformer_outputs = self.model.transformer(
+                inputs.to(self.device),
+                mems=labels,
+                head_mask=None,
+                inputs_embeds=None,
+                output_attentions=None,
+                output_hidden_states=None,
+                return_dict=None,
+            )
+            last_hidden = transformer_outputs[0]
+            pred_hidden = last_hidden[:, -inputs.size(1):] 
+            proj_out = self.model.out_layer(pred_hidden)
+            proj_out = proj_out.view(-1, self.model.config.vocab_size, inputs.size(1))
+            loss = self.model.crit(proj_out, labels.to(self.device))
+            mems = transformer_outputs.mems
+            return loss, mems
+
+    def __save_model(self, epoch):
         print('--- Saving model ---')
-        self.model.save_pretrained('Transformer-XL')
+        state = {
+            'epoch': str(epoch),
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optim.state_dict(),
+        }
+        torch.save(state, os.path.join(self.save_folder, f'model_epoch_{state["epoch"]}.pth'))
         print('--- Model saved ---')
 
     def fit(self):
-        for epoch in range(self.epochs):
-            print(f'--- Epoch {epoch+1}/{self.epochs} ---')
+        for epoch in range(self.epochs): 
+            print(f'--- Epoch {epoch+1}/{self.epochs} ---') 
             pbar = tqdm(self.trainloader)
-            for batch, data in enumerate(pbar):
+            for batch, data in enumerate(pbar): 
                 mems = None #init mems as None
-                input_chunks = torch.split(data, self.b_chunks, 1)
-                tgt_chunks = torch.split(data, self.b_chunks, 1)
+                inputs, labels = data
+                input_chunks = torch.split(inputs, self.b_chunks, 1)
+                tgt_chunks = torch.split(labels, self.b_chunks, 1) 
+       
                 losses = []
-                #print(len(input_chunks))
+       
                 for i in range(len(input_chunks)):
-                    inp_i = input_chunks[i].contiguous()
+                    inp_i = input_chunks[i].contiguous() 
                     tgt_i = tgt_chunks[i].contiguous()
-
-                    #print(inp_i.size(1))
                     if inp_i.size(1) == 0:
                       continue #skip if empty
-                      
-                    self.optim.zero_grad() #clear gradients
-                    out = self.model(inp_i.to(self.device), labels=tgt_i.to(self.device), mems=mems)
-                    loss, _, mem = out[0], out[1], out[2]
-                    mems = mem
-                    loss = loss.mean()
+           
+                    loss, mems = self.__forward(inp_i, tgt_i)
+
                     loss.backward()
-                    self.optim.step()
-                    losses.append(float(loss.detach().cpu().numpy()))
+                    
+                    if (i+1) % self.accum == 0: # accumulate gradients for BPTT # % is modulo operator, divides right by left and returns remainder
+                        self.optim.step()
+                        self.optim.zero_grad() #clear gradients
+
+                    losses.append(float(loss.detach().cpu().numpy())) # just use .item() ?
+
                 if(len(losses)!=0):
                   mean_loss = sum(losses)/len(losses)
                   pbar.set_description(f'loss: {mean_loss}')
                   self.losses.append(mean_loss)
+
+            val_losses = []
+            for batch, data in enumerate(self.valloader):
+                inputs, labels = data
+                input_chunks = torch.split(inputs, self.b_chunks, 1)
+                tgt_chunks = torch.split(labels, self.b_chunks, 1) 
+       
+                for i in range(len(input_chunks)):
+                    inp_i = input_chunks[i].contiguous() 
+                    tgt_i = tgt_chunks[i].contiguous()
+                    if inp_i.size(1) == 0:
+                      continue
+                    with torch.no_grad():
+                        loss, mems = self.__forward(inp_i, tgt_i)
+                    losses.append(float(loss.detach().cpu().numpy()))
+            mean_val_loss = sum(val_losses)/len(val_losses)
+            print(f'Val_loss: {mean_val_loss}')
+
+            self.__save_model(epoch)
+                  
         print('--- Training finished ---')
-        self.onTrainEnd()
+        self.__save_model('final')
         
 

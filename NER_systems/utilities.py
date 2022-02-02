1f806f114899e46309b917cfdd0b692168dccddb
dawidgrad <dawidgrad.work@gmail.com>
2022-02-02 03:11:46 +0000

    Modified utilities script to handle multiple methods of transcript retrieval

--- NER_systems/utilities.py
+++ NER_systems/utilities.py
@@ -1,20 +1,42 @@
 import os
 import pandas as pd
+import json
+from enum import Enum
 
-def get_transcript(directory):    
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file == 'John Roche.tsv':
-                df = pd.read_csv("{}/{}".format(directory, file), sep='\t')
-                cond = (df['Speaker'] != 'New Film') & (df['Speaker'] != 'End of audio')
-                section_list = list(df[cond]['Transcript'])
+class TranscriptType(Enum):
+    ANNOTATION = 1
+    OUTPUT = 2
+    TEST = 3
 
-                consecutive_n = 5
-                temp = '{} ' * consecutive_n
-                full_transcript = [temp.format(*item) for item in zip(*[iter(section_list)] * consecutive_n)] 
-                break
+# Get the appropriate transcript based on the type of input we want to use
+def get_transcript(directory, type):
+    result = []
 
-    return full_transcript
+    if type == TranscriptType.ANNOTATION:
+        with open('./ner_annotations.jsonl', 'r') as json_file:
+            json_list = list(json_file)
+        
+        for json_str in json_list:
+            document = json.loads(json_str)
+            result.append(document['data'])
+
+    elif type == TranscriptType.OUTPUT:
+        # TODO
+        result = []
+    elif type == TranscriptType.TEST:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file == 'John Roche.tsv':
+                    df = pd.read_csv("{}/{}".format(directory, file), sep='\t')
+                    cond = (df['Speaker'] != 'New Film') & (df['Speaker'] != 'End of audio')
+                    section_list = list(df[cond]['Transcript'])
+
+                    consecutive_n = 5
+                    temp = '{} ' * consecutive_n
+                    result = [temp.format(*item) for item in zip(*[iter(section_list)] * consecutive_n)] 
+                    break
+
+    return result
 
 def write_to_file(directory, data):
     with open(directory, "w") as file:
